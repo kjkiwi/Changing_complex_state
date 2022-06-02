626a2fd731d04e66554c3f085238fe34701c5d36
Minty <82933018+kjkiwi@users.noreply.github.com>
2022-06-02 12:28:56 +1200

    First method Updating heading with data typed into fName and lName inputs.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [firstName, setFirstName] = useState("");
+  const [fNameHeading, setFNameHeading] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [lNameHeading, setLNameHeading] = useState("");
+
+  function captureFName(event) {
+    setFirstName(event.target.value);
+  }
+
+  function captureLName(event) {
+    setLastName(event.target.value);
+  }
+
+  function pushAction() {
+    setFNameHeading(firstName);
+    setLNameHeading(lastName);
+    event.preventDefault();
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <button>Submit</button>
+      <h1>
+        Hello {fNameHeading} {lNameHeading}{" "}
+      </h1>
+      <form onSubmit={pushAction}>
+        <input
+          onChange={captureFName}
+          name="fName"
+          placeholder="First Name"
+          value={firstName}
+        />
+        <input
+          onChange={captureLName}
+          name="lName"
+          placeholder="Last Name"
+          value={lastName}
+        />
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
